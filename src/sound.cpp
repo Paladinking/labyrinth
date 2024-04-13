993dc5dff03d38f77d9be06d98c1892ad5d495cb
Nils Hammarberg <nilha958@student.liu.se>
2024-04-13 15:36:04 +0200

    Sound_ex might work now

---
+++ src/sound.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "sound.h"
+
+//------------------------------------------------------------------------------------
+// Program main entry point
+//------------------------------------------------------------------------------------
+
+MySound::MySound(std::vector<Sound> sounds) : sounds(sounds), index(0) {};
+
+
+//Determines if the previos sound is done playing
+bool IsPrevPlaying(std::vector<Sound> v, int s) {
+    if(s < 1) {
+        return IsSoundPlaying(v[v.size() - 1]);
+    }else {
+        return IsSoundPlaying(v[s - 1]);
+    }
+}
+
+
+void MySound::PlayMySound() {
+    if(IsPrevPlaying(sounds, index)) {
+        return;
+    }
+    PlaySound(sounds[index]);
+    if(index >= sounds.size() - 1) {
+        index = 0;
+    }else {
+        index++;
+    }
+
+}
+
+
