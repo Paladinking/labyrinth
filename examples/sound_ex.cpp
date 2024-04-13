dbfbc89dd117a3a71b61283ca6fd615b903ba641
Nils Hammarberg <nilha958@student.liu.se>
2024-04-13 14:42:21 +0200

    Created file for sounds, and a sound class with index. Example file for testing sounds

--- examples/sound_ex.cpp
+++ examples/sound_ex.cpp
@@ -19,46 +19,13 @@ int main(void)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
-    bool pause = false;
-
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
-
-    Vector2 ballPosition = { GetScreenWidth()/2.0f, GetScreenHeight()/2.0f };
-    Vector2 ballSpeed = { 5.0f, 4.0f };
-    int ballRadius = 20;
-
     InitAudioDevice();  
-
-    Music music = LoadMusicStream("resources/Rain.mp3");
-    music.looping = false;
-    float pitch = 1.0f;
-
     std::vector<Sound> steps = { LoadSound("resources/Step1.mp3"), LoadSound("resources/Step2.mp3") , LoadSound("resources/Step3.mp3")};
     int step = 0;
     bool walking = false;
-
-    //PlayMusicStream(music);
-
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
-       /*
-        ballPosition.x += ballSpeed.x;
-        ballPosition.y += ballSpeed.y;
-
-        // Check walls collision for bouncing
-        if ((ballPosition.x >= (GetScreenWidth() - ballRadius)) || (ballPosition.x <= ballRadius)) ballSpeed.x *= -1.0f;
-        if ((ballPosition.y >= (GetScreenHeight() - ballRadius)) || (ballPosition.y <= ballRadius)) ballSpeed.y *= -1.0f;
-
-        */
         //Music
         UpdateMusicStream(music);      // Update music buffer with new stream data
         // Restart music playing (stop and play)
