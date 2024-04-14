9e4a694914f66126d793f3dca3dd45839109e464
Pontus <ponpo202@student.liu.se>
2024-04-14 12:39:46 +0200

    added glsl100 resource

--- resources/shaders/glsl100/lighting.fs
+++ resources/shaders/glsl100/lighting.fs
@@ -1,19 +1,18 @@
-#version 330
+#version 100
+
+precision mediump float;
 
 // Input vertex attributes (from vertex shader)
-in vec3 fragPosition;
-in vec2 fragTexCoord;
-//in vec4 fragColor;
-in vec3 fragNormal;
+varying vec3 fragPosition;
+varying vec2 fragTexCoord;
+varying vec4 fragColor;
+varying vec3 fragNormal;
 
 // Input uniform values
 uniform sampler2D texture0;
 uniform sampler2D texture1;
 uniform sampler2D texture2;
 uniform vec4 colDiffuse;
-
-// Output fragment color
-out vec4 finalColor;
 
 // NOTE: Add here your custom variables
 
@@ -37,10 +36,10 @@ uniform vec3 viewPos;
 void main()
 {
     // Texel color fetching from texture sampler
-    vec4 texelColor = texture(texture0, fragTexCoord);
+    vec4 texelColor = texture2D(texture0, fragTexCoord);
     vec3 lightDot = vec3(0.0);
     vec3 inNormal = normalize(fragNormal);
-    vec3 mapNormal = normalize(texture(texture2, fragTexCoord).xyz);
+    vec3 mapNormal = normalize(texture2D(texture2, fragTexCoord).xyz);
     vec3 nMapOffset = vec3(0.5, 0.41, 0.8);
     vec3 normal = normalize(inNormal + mapNormal - nMapOffset);
     // vec3 normal = inNormal;
@@ -54,7 +53,7 @@ void main()
         if (lights[i].enabled == 1)
         {
             vec3 light = vec3(0.0);
-            float lightDist = 1.0;
+            // float lightDist = 1.0;
 
             if (lights[i].type == LIGHT_DIRECTIONAL)
             {
@@ -68,7 +67,7 @@ void main()
                 light = normalize(light);
             }
 
-            float NdotL = max(dot(normal, light), 0.0)/lightDist;
+            float NdotL = max(dot(normal, light), 0.0);
             lightDot += lights[i].color.rgb*NdotL;
 
             float specCo = 0.0;
@@ -77,9 +76,9 @@ void main()
         }
     }
 
-    finalColor = (texelColor*((colDiffuse + vec4(specular, 1.0))*vec4(lightDot, 1.0)));
-    finalColor += texelColor*(ambient/10.0)*colDiffuse;
+    vec4 finalColor = (texelColor*((colDiffuse + vec4(specular, 1.0))*vec4(lightDot, 1.0)));
+    finalColor += texelColor*(ambient/10.0);
 
     // Gamma correction
-    finalColor = pow(finalColor, vec4(1.0/2.2));
+    gl_FragColor = pow(finalColor, vec4(1.0/2.2));
 }
